f373addff018c9e2365899bd85b60dbbeaff24ee
RafaelCardoso <cardosorafael@outlook.com.br>
2014-03-25 16:08:26 -0300

    Initial Commit

---
+++ appCrianca/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  appCrianca
+//
+//  Created by RAFAEL CARDOSO DA SILVA on 25/03/14.
+//  Copyright (c) 2014 RAFAEL CARDOSO DA SILVA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ appCrianca/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  appCrianca
+//
+//  Created by RAFAEL CARDOSO DA SILVA on 25/03/14.
+//  Copyright (c) 2014 RAFAEL CARDOSO DA SILVA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
