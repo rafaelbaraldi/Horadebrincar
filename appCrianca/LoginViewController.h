ae78f2e189c38cac50b9869b8987b0f7eafb106f
RafaelCardoso <cardosorafael@outlook.com.br>
2014-04-17 14:22:51 -0300

    inicio login
    inicio login

---
+++ appCrianca/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  appCrianca
+//
+//  Created by RAFAEL CARDOSO DA SILVA on 16/04/14.
+//  Copyright (c) 2014 RAFAEL CARDOSO DA SILVA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ appCrianca/LoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.m
+//  appCrianca
+//
+//  Created by RAFAEL CARDOSO DA SILVA on 16/04/14.
+//  Copyright (c) 2014 RAFAEL CARDOSO DA SILVA. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
